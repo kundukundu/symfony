a9f79912f68c1cc4aa81dd512cbcaadd053b9c49
Chiyana Simões <csimoes@inusual.com>
2018-06-30 18:27:26 +0200

    Download File
    Download File Action

---
+++ ficheroAction.php
@@ -0,0 +1,20 @@
+<?php
+
+//...
+public function ficheroAction($nobreFichero)
+{
+   $kernel = $this->get('kernel');
+   $path = $kernel->locateResource('@AppBundle/Resources/ficheros/'.$nombreFichero);
+
+   $response = new BinaryFileResponse($path)
+   $response->headers->set('Content-Type', 'application/pdf');
+   /** Cargar el fichero en el navegador
+   * //Utiliza ResponseHeaderBag::DISPOSITION_ATTACHMENT para guardar el fichero como adjunto
+   */
+   $response->setContentDisposition(
+      ResponseHeaderBag::DISPOSITION_INLINE,
+      $nombreFichero
+   );
+
+   return $response;
+}
